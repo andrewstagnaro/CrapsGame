f99d4926b102f7aa2c08942eb1728328046e1979
andrewstagnaro <42977270+andrewstagnaro@users.noreply.github.com>
2018-10-23 13:37:04 -0400

    Add files via upload

---
+++ Craps/src/Craps.java
@@ -0,0 +1,23 @@
+/**
+ * 
+ * @author Drew Stagnaro
+ * Craps Game Class
+ * pd 6
+ *
+ */
+public class Craps
+{
+	public static void main(String[] args)
+	{
+	//Fields
+	/**
+	 * Creates new Dice
+	 */
+	Dice die1 = new Dice();
+	Dice die2 = new Dice();
+	System.out.println("Welcome To The Craps Game");
+	System.out.println("Please Press 'Enter' to Start");
+	
+	
+	}
+}

e9067d6d1d95d970bc0e5d3bc4747a2ca40fcf65
andrewstagnaro <42977270+andrewstagnaro@users.noreply.github.com>
2018-10-25 13:37:46 -0400

    Add files via upload

--- Craps/src/Craps.java
+++ Craps/src/Craps.java
@@ -1,23 +1,67 @@
 /**
+/**
  * 
  * @author Drew Stagnaro
  * Craps Game Class
  * pd 6
  *
  */
-public class Craps
+import java.util.Scanner;
+
+public class Craps 
 {
-	public static void main(String[] args)
+	public static void main(String[] args) 
 	{
-	//Fields
-	/**
-	 * Creates new Dice
-	 */
-	Dice die1 = new Dice();
-	Dice die2 = new Dice();
-	System.out.println("Welcome To The Craps Game");
-	System.out.println("Please Press 'Enter' to Start");
-	
-	
+		/**
+		 * Creates new Dice
+		 */
+		Dice die1 = new Dice();
+		boolean playAgain = true;
+		while (playAgain)
+		{
+		System.out.println("Welcome To The Craps Game");
+		System.out.print("Please Press 'Enter' to Roll");
+		Scanner scanner = new Scanner(System.in);
+		scanner.nextLine();
+		int point = die1.roll() + die1.roll();
+		System.out.print(point);
+		if (point == 7 || point == 11) 
+		{
+			System.out.print(" Congradulations! You have won the game!");
+		} 
+		else if (point == 2 || point == 12) 
+		{
+			System.out.print("Sorry, you lost.");
+		}
+		else 
+		{
+			System.out.print(" Please press 'enter' roll again.");
+			scanner.nextLine();
+			int rollValue = die1.roll() + die1.roll();
+			while (rollValue != point && rollValue != 7)
+			{
+			System.out.println(rollValue);
+			System.out.print("Please press 'enter' roll again.");
+			scanner.nextLine();
+			rollValue = die1.roll() + die1.roll();
+			}
+			System.out.println(rollValue);
+			if(rollValue == point) {
+				System.out.print(" Congradulations! You have won the game!");
+			} 
+			else {
+				System.out.print("Sorry, you lost.");
+			}
+		}
+		System.out.print("Would you like to play again?");
+		scanner.nextLine();
+		while ()
+		{
+			
+		}
+			
+		}
+		
+		
 	}
 }
